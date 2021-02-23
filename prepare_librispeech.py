87e6caa98f1809dfcbfe792b569435ead7c39f4f
menDDang <menddang2357@gmail.com>
2021-02-23 16:21:00 +0900

    add load_librispeech_dataset function

--- prepare_librispeech.py
+++ prepare_librispeech.py
@@ -24,3 +24,54 @@ def get_transcripts(data_dir, names):
     return transcripts
 
 
+def parse_line(line, data_dir, split_names):
+
+    line_split = tf.strings.split(line, ' ')
+
+    # Parse transcripts
+    uttid = line_split[0]
+    transcript = tf.py_function(
+        lambda x: b' '.join(x.numpy()).decode('utf8'),
+        inp=[line_split[1:]],
+        Tout=tf.string
+    )
+
+    # Parse audio file names
+    speaker_id, chapter_id, _ = tf.unstack(tf.strings.split(uttid, '-'), 3)
+    audio_file_paths = tf.map_fn(
+        lambda split_name: tf.strings.join([data_dir, split_name, speaker_id, chapter_id, uttid], '/') + '.flac',
+        tf.constant(split_names)
+    )
+
+    # Load audio files
+    audio, sr = tf.py_function(
+        lambda path: preprocess.load_audio(path[0].numpy()),
+        inp=[audio_file_paths],
+        Tout=[tf.float32, tf.int32]
+    )
+    
+    return audio, sr, transcript
+    
+
+def load_librispeech_dataset(data_dir, split_names):
+
+    transcript_file_paths = get_transcripts(data_dir, split_names)
+
+    # dataset : (transcript_file_path)
+    dataset = tf.data.TextLineDataset(transcript_file_paths)
+
+    # dataset : (audio, sr, transcript)
+    dataset = dataset.map(
+        lambda line: parse_line(line, data_dir, split_names),
+        num_parallel_calls=tf.data.experimental.AUTOTUNE
+    )
+
+    return dataset
+
+
+
+if __name__ == "__main__": 
+
+    dataset = load_librispeech_dataset("data/LibriSpeech", ["dev-clean"])
+    for b, inputs in enumerate(dataset.take(1)):
+        print(inputs)
