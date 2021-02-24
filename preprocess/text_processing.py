a70f3c858c17cbc011b7e586076c08dbeb71c5d6
menDDang <menddang2357@gmail.com>
2021-02-24 11:14:41 +0900

    add tokenize_text function

--- preprocess/text_processing.py
+++ preprocess/text_processing.py
@@ -1,14 +1,47 @@
+import os
+
 import tensorflow as tf
 
 
-def build_lookup_table(keys, values=None, default_value=0):
-    if values is None:
-        values = tf.range(len(keys))
+def normalize_text(text):
+
+    text = text.lower()
+    text = text.replace('"', '')
+
+    return text
+
+def create_char_tokenizer():
+
+    alphabet = "abcdefghijklmnopqrstuvwxyz'"
+    end = '</s>'  # this token will be 0. Note that 0 means end of sentence
+    keys = [end, ' '] + [c for c in alphabet]
+
+    values = range(len(keys))
 
     init_key_values = tf.lookup.KeyValueTensorInitializer(keys=keys, values=values)
 
-    hash_table = tf.lookup.StaticHashTable(init_key_values, default_value=default_value)
+    hash_table = tf.lookup.StaticHashTable(init_key_values, default_value=0)
 
-    return hash_table
+    return lambda text: hash_table.lookup(tf.strings.bytes_split(text))
 
 
+def tokenize_text(text, tokenizer):
+
+    def _normalize(_text):
+        return tf.py_function(
+        lambda x: normalize_text(x.numpy().decode('utf8')),
+        inp=[_text],
+        Tout=tf.string)
+
+    normalized_text = _normalize(text)
+    tokens = tokenizer(tf.reshape(normalized_text, ()))
+
+    return tokens
+
+if __name__ == "__main__":
+
+    tokenizer = create_char_tokenizer()
+    
+    text = "apple"
+    tokens = tokenize_text(text, tokenizer)
+    print(tokens)
