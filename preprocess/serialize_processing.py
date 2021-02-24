71f18dabf8b37a7b904e1f63d0c6b3933c7811b7
menDDang <menddang2357@gmail.com>
2021-02-24 16:32:32 +0900

    init commit

---
+++ preprocess/serialize_processing.py
@@ -0,0 +1,52 @@
+import tensorflow as tf
+
+
+def serialize_example(mel, tokens):
+
+    def _bytes_features(value):
+        if isinstance(value, type(tf.contant(0))):
+            value = value.numpy()
+        return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+    def _serialize(_mel, _tokens):
+        serialized_mel = tf.io.serialize_tensor(_mel)
+        serialized_tokens = tf.io.serialize_tensor(_tokens)
+
+        feature = {
+            'mel': _bytes_features(serialized_mel),
+            'tokens': _bytes_features(serialized_tokens)
+        }
+
+        example = tf.train.Example(features=tf.train.Features(feature=feature))
+        example = example.SerializeToString()
+        return example
+
+    output = tf.py_function(
+        _serialize,
+        inp=[mel, tokens],
+        Tout=[tf.string])
+
+    return tf.reshape(output, ())
+
+
+def parse_example(serialized_example):
+
+    parse_dict = {
+        'mel': tf.io.FixedLenFeature([], tf.string),
+        'tokens': tf.io.FixedLenFeature([], tf.string)
+    }
+
+    example = tf.io.parse_single_example(serialized_example, parse_dict)
+
+    mel = tf.io.parse_tensor(example['mel'], out_type=tf.float32)
+    tokens = tf.io.parse_tensor(example['tokens'], out_type=tf.int32)
+
+    return (mel, tokens)
+
+
+def write_dataset(dataset, path):
+
+    writer = tf.data.experimental.TFRecordWriter(path)
+    writer.write(dataset)
+
+    
