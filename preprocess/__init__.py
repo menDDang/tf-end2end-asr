2c06a729da0026a3591f2e2c544e510f93b394d3
menDDang <menddang2357@gmail.com>
2021-02-24 11:39:22 +0900

    add compute_mel

--- preprocess/__init__.py
+++ preprocess/__init__.py
@@ -1,4 +1,7 @@
 from .signal_processing import load_audio
+from .signal_processing import compute_mel_bins
+from .signal_processing import compute_spectrum
+from .signal_processing import compute_mel
 
 from .text_processing import create_char_tokenizer
 from .text_processing import tokenize_text
